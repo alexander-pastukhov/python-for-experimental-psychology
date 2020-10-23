284092040ee4c0d7c2f2b998c1c5fff1a9a109e4
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-23 12:28:16 +0200

    Generated book

---
+++ docs/other/empty.py
@@ -0,0 +1,3 @@
+# I am an empty file with .py extension
+# If you open me in VS Code, it will suggest install Python extension, if you don't already have one.
+# Follow the advice and install it! And install a linter, never a bad idea!
